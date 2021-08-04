6b58ffedc9ce5d66dc5a459b7364da4d017f7ae2
Moritz Johner <beller.moritz@googlemail.com>
2021-08-04 15:18:56 +0200

    feat: add vault e2e tests

--- e2e/suite/vault/provider.go
+++ e2e/suite/vault/provider.go
@@ -31,36 +31,40 @@ import (
 	esv1alpha1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1alpha1"
 	esmeta "github.com/external-secrets/external-secrets/apis/meta/v1"
 	"github.com/external-secrets/external-secrets/e2e/framework"
+	"github.com/external-secrets/external-secrets/e2e/framework/addon"
 )
 
 type vaultProvider struct {
 	url       string
-	token     string
 	client    *vault.Client
 	framework *framework.Framework
 }
 
-func newVaultProvider(f *framework.Framework, url, token string) *vaultProvider {
-	vc, err := vault.NewClient(&vault.Config{
-		Address: url,
-	})
-	Expect(err).ToNot(HaveOccurred())
-	vc.SetToken(token)
+const (
+	certAuthProviderName    = "cert-auth-provider"
+	appRoleAuthProviderName = "app-role-provider"
+	kvv1ProviderName        = "kv-v1-provider"
+	jwtProviderName         = "jwt-provider"
+)
 
+func newVaultProvider(f *framework.Framework) *vaultProvider {
 	prov := &vaultProvider{
 		framework: f,
-		url:       url,
-		token:     token,
-		client:    vc,
 	}
 	BeforeEach(prov.BeforeEach)
 	return prov
 }
 
+// CreateSecret creates a secret in both kv v1 and v2 provider.
 func (s *vaultProvider) CreateSecret(key, val string) {
 	req := s.client.NewRequest(http.MethodPost, fmt.Sprintf("/v1/secret/data/%s", key))
 	req.BodyBytes = []byte(fmt.Sprintf(`{"data": %s}`, val))
 	_, err := s.client.RawRequestWithContext(context.Background(), req)
+	Expect(err).ToNot(HaveOccurred())
+
+	req = s.client.NewRequest(http.MethodPost, fmt.Sprintf("/v1/secret_v1/%s", key))
+	req.BodyBytes = []byte(val)
+	_, err = s.client.RawRequestWithContext(context.Background(), req)
 	Expect(err).ToNot(HaveOccurred())
 }
 
@@ -68,17 +72,40 @@ func (s *vaultProvider) DeleteSecret(key string) {
 	req := s.client.NewRequest(http.MethodDelete, fmt.Sprintf("/v1/secret/data/%s", key))
 	_, err := s.client.RawRequestWithContext(context.Background(), req)
 	Expect(err).ToNot(HaveOccurred())
+
+	req = s.client.NewRequest(http.MethodDelete, fmt.Sprintf("/v1/secret_v1/%s", key))
+	_, err = s.client.RawRequestWithContext(context.Background(), req)
+	Expect(err).ToNot(HaveOccurred())
 }
 
 func (s *vaultProvider) BeforeEach() {
+	ns := s.framework.Namespace.Name
+	v := addon.NewVault(ns)
+	s.framework.Install(v)
+	s.client = v.VaultClient
+	s.url = v.VaultURL
+
+	s.CreateCertStore(v, ns)
+	s.CreateTokenStore(v, ns)
+	s.CreateAppRoleStore(v, ns)
+	s.CreateV1Store(v, ns)
+	s.CreateJWTStore(v, ns)
+}
+
+func (s *vaultProvider) CreateCertStore(v *addon.Vault, ns string) {
 	By("creating a vault secret")
+	clientCert := v.ClientCert
+	clientKey := v.ClientKey
+	serverCA := v.VaultServerCA
 	vaultCreds := &v1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "provider-secret",
-			Namespace: s.framework.Namespace.Name,
+			Name:      certAuthProviderName,
+			Namespace: ns,
 		},
-		StringData: map[string]string{
-			"token": s.token, // vault dev-mode default token
+		Data: map[string][]byte{
+			"token":       []byte(v.RootToken),
+			"client_cert": clientCert,
+			"client_key":  clientKey,
 		},
 	}
 	err := s.framework.CRClient.Create(context.Background(), vaultCreds)
@@ -87,19 +114,192 @@ func (s *vaultProvider) BeforeEach() {
 	By("creating an secret store for vault")
 	secretStore := &esv1alpha1.SecretStore{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      s.framework.Namespace.Name,
-			Namespace: s.framework.Namespace.Name,
+			Name:      certAuthProviderName,
+			Namespace: ns,
 		},
 		Spec: esv1alpha1.SecretStoreSpec{
 			Provider: &esv1alpha1.SecretStoreProvider{
 				Vault: &esv1alpha1.VaultProvider{
-					Version: esv1alpha1.VaultKVStoreV2,
-					Path:    "secret",
-					Server:  s.url,
+					Version:  esv1alpha1.VaultKVStoreV2,
+					Path:     "secret",
+					Server:   s.url,
+					CABundle: serverCA,
+					Auth: esv1alpha1.VaultAuth{
+						Cert: &esv1alpha1.VaultCertAuth{
+							ClientCert: esmeta.SecretKeySelector{
+								Name: certAuthProviderName,
+								Key:  "client_cert",
+							},
+							SecretRef: esmeta.SecretKeySelector{
+								Name: certAuthProviderName,
+								Key:  "client_key",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	err = s.framework.CRClient.Create(context.Background(), secretStore)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func (s vaultProvider) CreateTokenStore(v *addon.Vault, ns string) {
+	serverCA := v.VaultServerCA
+	vaultCreds := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "token-provider",
+			Namespace: ns,
+		},
+		Data: map[string][]byte{
+			"token": []byte(v.RootToken),
+		},
+	}
+	err := s.framework.CRClient.Create(context.Background(), vaultCreds)
+	Expect(err).ToNot(HaveOccurred())
+	secretStore := &esv1alpha1.SecretStore{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      s.framework.Namespace.Name,
+			Namespace: ns,
+		},
+		Spec: esv1alpha1.SecretStoreSpec{
+			Provider: &esv1alpha1.SecretStoreProvider{
+				Vault: &esv1alpha1.VaultProvider{
+					Version:  esv1alpha1.VaultKVStoreV2,
+					Path:     "secret",
+					Server:   s.url,
+					CABundle: serverCA,
 					Auth: esv1alpha1.VaultAuth{
 						TokenSecretRef: &esmeta.SecretKeySelector{
-							Name: "provider-secret",
+							Name: "token-provider",
 							Key:  "token",
+						},
+					},
+				},
+			},
+		},
+	}
+	err = s.framework.CRClient.Create(context.Background(), secretStore)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func (s vaultProvider) CreateAppRoleStore(v *addon.Vault, ns string) {
+	By("creating a vault secret")
+	serverCA := v.VaultServerCA
+	vaultCreds := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      appRoleAuthProviderName,
+			Namespace: ns,
+		},
+		Data: map[string][]byte{
+			"approle_secret": []byte(v.AppRoleSecret),
+		},
+	}
+	err := s.framework.CRClient.Create(context.Background(), vaultCreds)
+	Expect(err).ToNot(HaveOccurred())
+
+	By("creating an secret store for vault")
+	secretStore := &esv1alpha1.SecretStore{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      appRoleAuthProviderName,
+			Namespace: ns,
+		},
+		Spec: esv1alpha1.SecretStoreSpec{
+			Provider: &esv1alpha1.SecretStoreProvider{
+				Vault: &esv1alpha1.VaultProvider{
+					Version:  esv1alpha1.VaultKVStoreV2,
+					Path:     "secret",
+					Server:   s.url,
+					CABundle: serverCA,
+					Auth: esv1alpha1.VaultAuth{
+						AppRole: &esv1alpha1.VaultAppRole{
+							Path:   v.AppRolePath,
+							RoleID: v.AppRoleID,
+							SecretRef: esmeta.SecretKeySelector{
+								Name: appRoleAuthProviderName,
+								Key:  "approle_secret",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	err = s.framework.CRClient.Create(context.Background(), secretStore)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func (s vaultProvider) CreateV1Store(v *addon.Vault, ns string) {
+	serverCA := v.VaultServerCA
+	vaultCreds := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "v1-provider",
+			Namespace: ns,
+		},
+		Data: map[string][]byte{
+			"token": []byte(v.RootToken),
+		},
+	}
+	err := s.framework.CRClient.Create(context.Background(), vaultCreds)
+	Expect(err).ToNot(HaveOccurred())
+	secretStore := &esv1alpha1.SecretStore{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      kvv1ProviderName,
+			Namespace: ns,
+		},
+		Spec: esv1alpha1.SecretStoreSpec{
+			Provider: &esv1alpha1.SecretStoreProvider{
+				Vault: &esv1alpha1.VaultProvider{
+					Version:  esv1alpha1.VaultKVStoreV1,
+					Path:     "secret_v1", // see configure-vault.sh
+					Server:   s.url,
+					CABundle: serverCA,
+					Auth: esv1alpha1.VaultAuth{
+						TokenSecretRef: &esmeta.SecretKeySelector{
+							Name: "v1-provider",
+							Key:  "token",
+						},
+					},
+				},
+			},
+		},
+	}
+	err = s.framework.CRClient.Create(context.Background(), secretStore)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func (s vaultProvider) CreateJWTStore(v *addon.Vault, ns string) {
+	serverCA := v.VaultServerCA
+	vaultCreds := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "jwt-provider",
+			Namespace: ns,
+		},
+		Data: map[string][]byte{
+			"jwt": []byte(v.JWTToken),
+		},
+	}
+	err := s.framework.CRClient.Create(context.Background(), vaultCreds)
+	Expect(err).ToNot(HaveOccurred())
+	secretStore := &esv1alpha1.SecretStore{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      jwtProviderName,
+			Namespace: ns,
+		},
+		Spec: esv1alpha1.SecretStoreSpec{
+			Provider: &esv1alpha1.SecretStoreProvider{
+				Vault: &esv1alpha1.VaultProvider{
+					Version:  esv1alpha1.VaultKVStoreV2,
+					Path:     "secret",
+					Server:   s.url,
+					CABundle: serverCA,
+					Auth: esv1alpha1.VaultAuth{
+						Jwt: &esv1alpha1.VaultJwtAuth{
+							Role: v.JWTRole,
+							SecretRef: esmeta.SecretKeySelector{
+								Name: "jwt-provider",
+								Key:  "jwt",
+							},
 						},
 					},
 				},
